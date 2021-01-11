f6477dab935872ff37eb07f063010560345c283b
RuudvanderHagen <77159712+RuudvanderHagen@users.noreply.github.com>
2021-01-11 13:56:09 +0100

    Add files via upload

--- MLC Script.py
+++ MLC Script.py
@@ -13,6 +13,10 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.linear_model import LogisticRegression
+import math
+import cv2
+from os.path import join
+from os import getcwd
 
 ##
 
@@ -49,13 +53,12 @@ model.add(Dense(512, activation='relu'))
 model.add(Dense(unique_labels, activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-run_model = model.fit(X_train, y_train_hot, batch_size=256, epochs=50, verbose=1,
+run_model = model.fit(X_train, y_train_hot, batch_size=256, epochs=3, verbose=1,
                    validation_data=(X_val, y_val_hot))
 
 [test_loss, test_acc] = model.evaluate(X_test, y_test_hot)
 
 print("Results model with 4 layers: Loss = {}, accuracy = {}".format(test_loss, test_acc))
-
 
 ##
 
@@ -81,7 +84,7 @@ model2.add(Dense(26, activation='softmax')) #last layer
 
 optimizer = Adam(lr=0.001)
 model2.compile(optimizer=optimizer, loss = "categorical_crossentropy", metrics=['accuracy']) 
-model2.fit(x_train, Y_train, batch_size=32, epochs=50, verbose=1)
+model2.fit(x_train, Y_train, batch_size=32, epochs=3, verbose=1)
 
 y_pred = model2.predict(x_val)
 print('MAE:', mean_absolute_error(Y_val, y_pred).round(3))
@@ -99,6 +102,263 @@ print("Results: Loss = {}, accuracy = {}".format(test_loss, test_acc))
 
 ## Task two: Running on multiple letters within image
 
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    #return resized
+    # define padding 
+    if resized.shape[0] < height: 
+        pad = (height - resized.shape[0]) /2
+        tpad = math.ceil(pad) # round up
+        bpad = int(pad) # round down
+        lpad, rpad = 0,0 
+        
+    if resized.shape[1] < width:
+        pad = (width - resized.shape[1])/2
+        lpad = math.ceil(pad) # round up
+        rpad = int(pad)
+        tpad, bpad = 0, 0 
+        
+    image = cv2.copyMakeBorder(resized, tpad, bpad, lpad, rpad, # top, bottom, left, right,
+        cv2.BORDER_CONSTANT)
+   
+    return image
+
+def find_contour(dic): #dictionary with rectangle contour & index as key
+    
+    #Step 2: sort dictionary with entry with longest width
+        ###NOTE: Assumption - Overlapping letters are of longest width 
+    longest_width = sorted(dic.values(), key=lambda x:x[2])[-1] #longest one last 
+
+    #Step 3: Create new contours 
+    new_contours = {}
+    counter = 0 
+
+    for ind in range( len(dic) ): 
+
+        #separate letters 
+        if dic[ind] == longest_width: 
+
+            #setup
+            x, y, w, h = dic[ind] 
+
+            #first letter 
+            r1 = x, y, int(w/2), h 
+            new_contours[counter] = r1
+            counter +=1 
+
+            #second letter 
+            r2 = int(x + w/2), y, int(w/2), h
+            new_contours[counter] = r2     
+            counter +=1 
+            
+
+        else: 
+            new_contours[counter]= dic[ind]
+            counter +=1   
+                   
+    return new_contours # contours_found (for testing > shows change)
+
+def unwanted(dic): #List of array of contours// assumes contour more than 4 
+        
+    # list of array by how many too long 
+    no_excess = len(dic) - 4
+    
+    #list showing array and size of respective array
+    l = []
+    for contour in dic.values():
+        
+        #setup
+        x, y, w, h = contour
+
+        l.append( (contour, (w*h)) )
+        
+
+    #create new list with excess array
+    excess =  sorted(l, key=lambda x:x[-1])[:no_excess]
+    re_list = []
+    
+    for item in excess:
+        
+        re_list.append(item[0])
+
+    #Step 3: Create new contours 
+    new_contours = {}
+    counter = 0 
+
+    for val in dic.values():
+        
+        if val in re_list: 
+            
+            continue 
+            
+        else: 
+            new_contours[counter] = val
+            counter +=1
+            
+    return new_contours
+
+def find_4contours(contours_found): # dic with index keys 
+    
+    if len( contours_found ) < 4:
+            
+            new_contours = find_contour(contours_found) # here recursion would be good 
+            
+    else:
+            
+            new_contours = unwanted(contours_found) #removes excess array based on size
+            
+    return new_contours
+
+def check(contours_found):
+    
+    if len(contours_found) == 4: 
+        
+        return contours_found
+    
+    else: 
+        return check(find_4contours(contours_found))
+    
+
+#path = join( getcwd(), 'data', 'test-dataset.npy' ) 
+captchas = np.load("test-dataset.npy")
+
+#iterate through case by case: 
+final_pred = []
+css = []
+
+for exp, captcha in enumerate (captchas): 
+    
+    #------format captcha image-----#
+    
+    ##add extra padding around the image
+    pad = cv2.copyMakeBorder(captcha, 8, 8, 8, 8, cv2.BORDER_REPLICATE)
+    
+    ## blur 
+    blur = cv2.medianBlur(pad , 3)
+    
+    ##thresholding 
+    _, thresh = cv2.threshold(blur,127,255, cv2.THRESH_BINARY+cv2.THRESH_OTSU) #127 middle between white/black
+    
+    
+    #------contour extraction-----#
+   
+    ##detect contours of all images 
+    contours, hierachy = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    
+    #print (contours, f"hierachy: {hierachy}")
+    #------contour filtration-----#
+    
+    ##contours smaller than 30 pixels excluded
+            ##NOTE - replacing pixel with black may lead to imporvement
+    
+    contours_letters = []
+    
+    for contour in contours:
+        
+        css.append(contour.size)
+
+        if contour.size > 30: # check whats the best size 
+            
+            contours_letters.append(contour)
+            
+            
+    #------letter extraction-----#  
+    
+        #Prep: Build dic with contours and index as key
+        
+    contours_extracted = [] #stores contours found: rectangle
+                        #e.g. [(103, 9, 20, 17), ...]
+    
+    for i in range( len(contours_letters) ):
+        
+        contours_extracted.append(cv2.boundingRect(contours_letters[i]))
+        
+        
+    #------sort contours-----# 
+     ## sorted in accordance with x position
+    sort_x = sorted(contours_extracted, key=lambda x: x[0])
+    
+    ##index key position
+    contours_found = {}
+    
+    for ind, contour in enumerate( sort_x ):
+        
+        contours_found[ind] = contour
+        
+    #--contour rectangle adjust--#
+    
+        #Main part: Finding relevant 4 contours if 
+        #we do not have more or less than 4 contours
+    
+    new_contours = check( contours_found ) #this works 
+        
+    #-------prediction-------
+    
+        #take dictionary with index as key in form {0: (103, 9, 20, 17), ...}
+    
+    captcha = ""
+    
+    for key in new_contours.keys():
+        
+        x, y, w, h = new_contours[key]
+        
+        # Extract the letter from the original image with a 2-pixel margin around the edge
+        #letter_image = pad[y - 2:y + h + 2, x - 2:x + w + 2]
+        letter_image = pad[y:y + h, x:x + w]
+        
+        #resize
+        l_I2828 = image_resize(letter_image, 28, 28)
+        #l_I2828 = imutils.resize(letter_image, width=28, height=28)
+        #l_I2828 = cv2.resize(letter_image, (28, 28), interpolation=cv2.INTER_AREA)
+                #interpolation=cv2.INTER_AREA ==> for decimating/ resizing image default INTER_LINEAR 
+                # https://www.geeksforgeeks.org/image-resizing-using-opencv-python/
+                #https://docs.opencv.org/3.4.0/da/d54/group__imgproc__transform.html#gga5bb5a1fea74ea38e1a5445ca803ff121acf959dca2480cc694ca016b81b442ceb
+                
+        
+        #Normalisation/ reshape
+        K = l_I2828/255.0  # Normalisation 
+        K = K.reshape(-1,28,28,1)
+        
+        #prediction
+        pred = model.predict(K)
+        
+        #append list 
+        letter = onehot.inverse_transform(pred)[0]
+        
+        #correct format
+        if letter < 10:
+            captcha += "0" + str( letter )
+        else:
+            captcha += str( letter ) 
+    
+    print(captcha)
+    final_pred.append(int(captcha))
 ##
 
 ## Extra: Predict function to use on trained models
