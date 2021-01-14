6238cd6b45bf850b2501619acf20d35e4c6622b5
xm3van <73356560+xm3van@users.noreply.github.com>
2021-01-14 14:19:29 +0000

    Update Combined  Filed - ML Challenge - Submission.py

--- Combined  Filed - ML Challenge - Submission.py
+++ Combined  Filed - ML Challenge - Submission.py
@@ -586,5 +586,5 @@ for captcha in captchas:
     
 ## Store pred in csv 
 csv_pred = np.array(final_pred)
-np.savetxt("predictions.csv", csv_pred, fmt="08%d", delimiter=",")
+np.savetxt("prediction.csv", csv_pred, fmt="08%d", delimiter=",")
 
