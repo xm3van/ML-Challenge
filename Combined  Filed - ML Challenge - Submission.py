72c881d2f449cb5815a1155c7fbc8266650a00cc
KirstenGoossens <77159881+KirstenGoossens@users.noreply.github.com>
2021-01-15 14:17:20 +0100

    Update Combined  Filed - ML Challenge - Submission.py

--- Combined  Filed - ML Challenge - Submission.py
+++ Combined  Filed - ML Challenge - Submission.py
@@ -162,46 +162,6 @@ print('Accuracy Validation:', accuracy_score(y_val, baseline.predict(x_val)).rou
 print("Results: Loss = {}, accuracy = {}".format(test_loss, test_acc))
 
 
-## Errors
-
-errors = (Y_pred_classes - Y_true != 0)
-
-Y_pred_classes_errors = Y_pred_classes[errors]
-Y_pred_errors = Y_pred[errors]
-Y_true_errors = Y_true[errors]
-X_val_errors = x_val[errors]
-
-def display_errors(errors_index,img_errors,pred_errors, obs_errors):
-    """ This function shows 6 images with their predicted and real labels"""
-    n = 0
-    nrows = 2
-    ncols = 3
-    fig, ax = plt.subplots(nrows,ncols,sharex=True,sharey=True)
-    for row in range(nrows):
-        for col in range(ncols):
-            error = errors_index[n]
-            ax[row,col].imshow((img_errors[error]).reshape((28,28)))
-            ax[row,col].set_title("Predicted label :{}\nTrue label :{}".format(pred_errors[error],obs_errors[error]))
-            n += 1
-
-# Probabilities of the wrong predicted numbers
-Y_pred_errors_prob = np.max(Y_pred_errors,axis = 1)
-
-# Predicted probabilities of the true values in the error set
-true_prob_errors = np.diagonal(np.take(Y_pred_errors, Y_true_errors, axis=1))
-
-# Difference between the probability of the predicted label and the true label
-delta_pred_true_errors = Y_pred_errors_prob - true_prob_errors
-
-# Sorted list of the delta prob errors
-sorted_dela_errors = np.argsort(delta_pred_true_errors)
-
-# Top 6 errors 
-most_important_errors = sorted_dela_errors[-6:]
-
-# Show the top 6 errors
-display_errors(most_important_errors, X_val_errors, Y_pred_classes_errors, Y_true_errors)
-
 #############################
 # # Task 2
 #############################
