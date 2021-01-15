b0cfe58cb0476fbad17ab936279dd57882778abd
RuudvanderHagen <77159712+RuudvanderHagen@users.noreply.github.com>
2021-01-15 11:37:31 +0100

    Update Combined  Filed - ML Challenge - Submission.py
    Removed plots

--- Combined  Filed - ML Challenge - Submission.py
+++ Combined  Filed - ML Challenge - Submission.py
@@ -104,17 +104,6 @@ run_model = model.fit(X_train, y_train_hot, batch_size=256, epochs=25, verbose=1
 
 print("Results multi level perceptron: Loss = {}, accuracy = {}".format(test_loss, test_acc))
 
-## Training and validation loss, training and validation accuracy
-
-fig, ax = plt.subplots(2,1)
-ax[0].plot(run_model.history['loss'], color='b', label="Training loss")
-ax[0].plot(run_model.history['val_loss'], color='r', label="validation loss",axes =ax[0])
-legend = ax[0].legend(loc='best', shadow=True)
-
-ax[1].plot(run_model.history['accuracy'], color='b', label="Training accuracy")
-ax[1].plot(run_model.history['val_accuracy'], color='r',label="Validation accuracy")
-legend = ax[1].legend(loc='best', shadow=True)
-
 ## 
 
 ## Model 2: Neural Network
@@ -172,43 +161,6 @@ print('Accuracy Validation:', accuracy_score(y_val, baseline.predict(x_val)).rou
 [test_loss, test_acc] = model2.evaluate(x_test, Y_test)
 print("Results: Loss = {}, accuracy = {}".format(test_loss, test_acc))
 
-## Confusion matrix
-
-def plot_confusion_matrix(cm, classes,
-                          normalize=False,
-                          title='Confusion matrix',
-                          cmap=plt.cm.Blues):
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, cm[i, j],
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
-
-# Predict the values from the validation dataset
-Y_pred = model2.predict(x_val)
-# Convert predictions classes to one hot vectors 
-Y_pred_classes = np.argmax(Y_pred,axis = 1) 
-# Convert validation observations to one hot vectors
-Y_true = np.argmax(Y_val,axis = 1) 
-# compute the confusion matrix
-confusion_mtx = confusion_matrix(Y_true, Y_pred_classes) 
-# plot the confusion matrix
-plot_confusion_matrix(confusion_mtx, classes = range(26)) 
 
 ## Errors
 
